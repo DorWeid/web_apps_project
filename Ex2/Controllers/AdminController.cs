7d4fc086cd25ecd7161de17afbc15324fd0a1faa
Matan <20matan@gmail.com>
2017-10-31 13:16:16 +0200

    Add admin panel + implement login

---
+++ Ex2/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ex2.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin/Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string username, string password)
+        {
+            var isAdmin = username == "matan" && password == "1234";
+            if (isAdmin)
+            {
+                System.Web.HttpContext.Current.Session["isAdmin"] = true;
+                return Json(new { succeeded = true });
+            }
+
+            //ViewBag.ErrMsg = "User name or password are incorrect.";
+            //return View();
+            return Json(new { error= "User name or password are incorrect." });
+            }
+    }
+}
